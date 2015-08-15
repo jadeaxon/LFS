ca77888fb7dcf409a2baf26c22b30ba941d3b1fb
Jeff Anderson <jadeaxon@hotmail.com>
2015-08-15 08:47:59 -0700

    Adds install-pkgs.sh and version-check.sh.

---
+++ install-pkgs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Installs extra pkgs needed on Ubuntu 14.10 to do Linux from Scratch.
+
+apt-get install gawk
+apt-get install makeinfo
+apt-get install texi2html
+apt-get install bison
+apt-get install texinfo
+
