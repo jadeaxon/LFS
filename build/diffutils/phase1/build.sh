6fdc4269b6e0637a045f280afbce6b484cf35077
Jeff Anderson <jadeaxon@hotmail.com>
2015-08-16 17:52:20 -0700

    Adds build for diffutils.

---
+++ build/diffutils/phase1/build.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+#==============================================================================
+# Variables
+#==============================================================================
+
+set -e
+
+src='../../../sources'
+src=$(cd $src; pwd) # Convert to absolute path.
+
+S=$(basename $0)
+
+bpkg=diffutils # Pkg base name.
+ver=3.3 # Pkg version.
+pkg=${bpkg}-${ver} # Full pkg name.
+z=xz # Compression style.
+tarball=${pkg}.tar.$z # Tarball filename.
+
+
+#==============================================================================
+# Main
+#==============================================================================
+
+# Extract source tarball.
+rm -rf $pkg/
+tar xavf $src/$tarball
+cd $pkg/
+
+# Apply the batch.
+if [ -f $src/${bpkg}*.patch ]; then
+	cp $src/${bpkg}*.patch .
+	patch -p1 < ${bpkg}*.patch
+fi
+
+./configure --prefix=/tools
+make
+make check
+make install
+
+cd ..
+rm -rf $pkg/
+echo "$S: You have build $pkg from scratch!"
+
+
