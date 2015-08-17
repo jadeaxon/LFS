3ca19dd7d9e079b1cc01890d1a23ad173aaed15b
Jeff Anderson <jadeaxon@hotmail.com>
2015-08-16 22:48:50 -0700

    Adds build for xz.

---
+++ build/xz/phase1/build.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+#==============================================================================
+# Variables
+#==============================================================================
+
+set -e
+
+src='../../../sources'
+src=$(cd $src; pwd) # Convert to absolute path.
+
+S=$(basename $0)
+
+bpkg=xz # Pkg base name.
+ver=5.2.0 # Pkg version.
+pkg=${bpkg}-${ver} # Full pkg name.
+z=xz # Compression style.
+tarball=${pkg}.tar.$z # Tarball filename.
+
+
+#==============================================================================
+# Main
+#==============================================================================
+
+# Extract source tarball.
+rm -rf $pkg/
+tar xavf $src/$tarball
+cd $pkg/
+
+# Apply the batch.
+if [ -f $src/${bpkg}*.patch ]; then
+	cp $src/${bpkg}*.patch .
+	patch -p1 < ${bpkg}*.patch
+fi
+
+./configure --prefix=/tools
+make
+make check
+make install
+
+cd ..
+rm -rf $pkg/
+echo "$S: You have built $pkg from scratch!"
+
+
