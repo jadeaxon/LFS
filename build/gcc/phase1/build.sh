6aa488f6257a395128175a49287aca07b47af3ec
Jeff Anderson <jeff.anderson@digecor.com>
2016-03-13 17:46:59 -0700

    Consolidate to single build.sh for gcc

--- build/gcc/phase1/build.sh
+++ build/gcc/phase1/build.sh
@@ -2,50 +2,26 @@
 
 set -e
 
+# TO DO: Update for LFS 7.9.
+
 # PRE: This script is run from the directory containing it.
 # PRE:
 # sed -i '/k prot/agcc_cv_libc_provides_ssp=yes' gcc/configure
 
 S=$(basename $0)
+# gcc-5.3.0.tar.bz2
+pkg=gcc-5.3.0
+src=/mnt/LFS/sources
+archive=$src/${pkg}.tar.bz2
 
-# TO DO: Extract archive here.
-# TO DO: Update for LFS 7.9.
+echo "$S: Building $pkg phase 1 for ${LFS_TGT}."
 
-../gcc-4.9.2/configure \
-	--target=$LFS_TGT \
-	--prefix=/tools \
-	--with-sysroot=$LFS \
-	--with-newlib \
-	--without-headers \
-	--with-local-prefix=/tools \
-	--with-native-system-header-dir=/tools/include \
-	--disable-nls \
-	--disable-shared \
-	--disable-multilib \
-	--disable-decimal-float \
-	--disable-threads \
-	--disable-libatomic \
-	--disable-libgomp \
-	--disable-libitm \
-	--disable-libquadmath \
-	--disable-libsanitizer \
-	--disable-libssp \
-	--disable-libvtv \
-	--disable-libcilkrts \
-	--disable-libstdc++-v3 \
-	--enable-languages=c,c++
-
-# Run this from the $LFS/sources/gcc-build/.
-# This should be an empty dir, not the one you just used to build gcc itself.
-../gcc-4.9.2/libstdc++-v3/configure \
-	--host=$LFS_TGT \
-	--prefix=/tools \
-	--disable-multilib \
-	--disable-shared \
-	--disable-nls \
-	--disable-libstdcxx-threads \
-	--disable-libstdcxx-pch \
-	--with-gxx-include-dir=/tools/$LFS_TGT/include/c++/4.9.2
+rm -rf $pkg
+tar xavf $archive
+echo "$S: Extracting archive $archive."
+cd $pkg
+# mkdir build
+# cd build
 
 # Prepares gcc for building (in a separate dir).
 # Run this script from the extracted tarball dir.
@@ -53,21 +29,21 @@ rm -rf mpfr
 rm -rf gmp
 rm -rf mpc
 
+# GCC depends on these pkgs.  We'll build them in place with the compiler.
 echo "$S: Including mpfr, gmp, and mpc."
-tar -xf ../mpfr-3.1.2.tar.xz
-mv -v mpfr-3.1.2 mpfr
-tar -xf ../gmp-6.0.0a.tar.xz
-mv -v gmp-6.0.0 gmp
-tar -xf ../mpc-1.0.2.tar.gz
-mv -v mpc-1.0.2 mpc
+tar xavf $src/mpfr-3.1.3.tar.xz
+mv -v mpfr-3.1.3 mpfr
+tar xavf $src/gmp-6.1.0.tar.xz
+mv -v gmp-6.1.0 gmp
+tar xavf $src/mpc-1.0.3.tar.gz
+mv -v mpc-1.0.3 mpc
 
 echo "$S: Patching mpfr."
 cd mpfr
-cp ../../mpfr*.patch .
-patch -p1 < mpfr*.patch
+patch -p1 < $src/mpfr*.patch
 cd -
 
-echo "$S: Forcing gcc to use LFS dynamic linker."
+echo "$S: Forcing gcc to use LFS dynamic linker from our tools/."
 for file in \
 	$(find gcc/config -name linux64.h -o -name linux.h -o -name sysv4.h)
 do
@@ -82,14 +58,52 @@ do
 	touch $file.orig
 done
 
-echo "$S: Fixing a 32-bit issue."
-sed -i '/k prot/agcc_cv_libc_provides_ssp=yes' gcc/configure
+# LFS 7.7
+# echo "$S: Fixing a 32-bit issue."
+# sed -i '/k prot/agcc_cv_libc_provides_ssp=yes' gcc/configure
 
+# Run this from the $LFS/sources/gcc-build/.
+# This should be an empty dir, not the one you just used to build gcc itself.
+rm -rf build
 echo "$S: Creating separate build dir."
-mkdir -v ../gcc-build
-cd ../gcc-build
-
+pwd
+mkdir -p build
+cd build
 echo "$S: gcc is now ready to be configured and built."
+
+# Configure gcc.
+../configure \
+	--target=$LFS_TGT \
+	--prefix=/tools \
+	--with-glibc-version=2.11 \
+	--with-sysroot=$LFS \
+	--with-newlib \
+	--without-headers \
+	--with-local-prefix=/tools \
+	--with-native-system-header-dir=/tools/include \
+	--disable-nls \
+	--disable-shared \
+	--disable-multilib \
+	--disable-decimal-float \
+	--disable-threads \
+	--disable-libatomic \
+	--disable-libgomp \
+	--disable-libquadmath \
+	--disable-libssp \
+	--disable-libvtv \
+	--disable-libstdcxx \
+	--enable-languages=c,c++
+
+# LSF 7.7
+# ../libstdc++-v3/configure \
+# 	--host=$LFS_TGT \
+# 	--prefix=/tools \
+# 	--disable-multilib \
+# 	--disable-shared \
+# 	--disable-nls \
+# 	--disable-libstdcxx-threads \
+# 	--disable-libstdcxx-pch \
+# 	--with-gxx-include-dir=/tools/$LFS_TGT/include/c++/4.9.2
 
 make
 make install
