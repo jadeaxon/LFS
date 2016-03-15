b515d45a996557f936081afe9b0e2af10876a3f8
Jeff Anderson <jeff.anderson@digecor.com>
2016-03-14 22:16:28 -0700

    Add build.sh for Expect in phase 2

---
+++ build/expect/phase2/build.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+#==============================================================================
+# Variables
+#==============================================================================
+
+set -e
+shopt -s nullglob
+
+S=$(basename $0)
+D=$(pwd)
+src=$LFS/sources
+
+pkg=expect5.45
+archive=( $src/${pkg}.tar.* ) # Expand glob into an array.
+archive=${archive[0]}
+
+
+#==============================================================================
+# Functions
+#==============================================================================
+
+extract_archive() {
+    echo "$S: Extracting archive $archive."
+    rm -rf $pkg
+    tar xavf $archive
+    cd $pkg
+}   
+
+apply_patch() {
+    patch=( $src/${pkg}*.patch )
+    patch=${patch[0]}
+    if [ "$patch" ]; then
+        echo "$S: Patching ${pkg} with ${patch}."
+        patch -p1 < $patch
+    fi
+}
+
+
+#==============================================================================
+# Main
+#==============================================================================
+
+extract_archive
+apply_patch
+
+# Make sure we use the right stty.
+cp -v configure{,.orig}
+sed 's:/usr/local/bin:/bin:' configure.orig > configure
+
+./configure --prefix=/tools \
+	--with-tcl=/tools/lib \
+	--with-tclinclude=/tools/include
+
+make
+make test
+make SCRIPTS="" install
+
+rm -rf $pkg
+echo "$S: Total victory!"
+
+
