ade0d796d804b5342729df605d41a5d3e1bb1505
Jeff Anderson <jeff.anderson@digecor.com>
2016-03-29 21:21:14 -0700

    Update tar build

--- build/tar/phase2/build.sh
+++ build/tar/phase2/build.sh
@@ -4,42 +4,27 @@
 # Variables
 #==============================================================================
 
-set -e
-
-src='../../../sources'
-src=$(cd $src; pwd) # Convert to absolute path.
-
-S=$(basename $0)
+source /usr/share/lib/bash-glory/LFS.sh
 
 bpkg=tar # Pkg base name.
 ver=1.28 # Pkg version.
-pkg=${bpkg}-${ver} # Full pkg name.
-z=xz # Compression style.
-tarball=${pkg}.tar.$z # Tarball filename.
+
+LFS::init
+LFS::debug
 
 
 #==============================================================================
 # Main
 #==============================================================================
 
-# Extract source tarball.
-rm -rf $pkg/
-tar xavf $src/$tarball
-cd $pkg/
-
-# Apply the batch.
-if [ -f $src/${bpkg}*.patch ]; then
-	cp $src/${bpkg}*.patch .
-	patch -p1 < ${bpkg}*.patch
-fi
+LFS::extract_archive
+LFS::apply_patch
 
 ./configure --prefix=/tools
 make
 make check
 make install
 
-cd ..
-rm -rf $pkg/
-echo "$S: You have built $pkg from scratch!"
+LFS::cleanup
 
 
