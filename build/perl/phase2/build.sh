37152c4bb49c49b1438c7eb6b9e28ba0ddc036b0
Jeff Anderson <jeff.anderson@digecor.com>
2016-03-29 17:29:56 -0700

    Update perl build

--- build/perl/phase2/build.sh
+++ build/perl/phase2/build.sh
@@ -4,43 +4,27 @@
 # Variables
 #==============================================================================
 
-set -e
-
-src='../../../sources'
-src=$(cd $src; pwd) # Convert to absolute path.
-
-S=$(basename $0)
+source /usr/share/lib/bash-glory/LFS.sh
 
 bpkg=perl # Pkg base name.
-ver=5.20.2 # Pkg version.
-pkg=${bpkg}-${ver} # Full pkg name.
-z=bz2 # Compression style.
-tarball=${pkg}.tar.$z # Tarball filename.
+ver=5.22.1 # Pkg version.
 
+LFS::init
+LFS::debug
 
 #==============================================================================
 # Main
 #==============================================================================
 
-# Extract source tarball.
-rm -rf $pkg/
-tar xavf $src/$tarball
-cd $pkg/
-
-# Apply the batch.
-if [ -f $src/${bpkg}*.patch ]; then
-	cp $src/${bpkg}*.patch .
-	patch -p1 < ${bpkg}*.patch
-fi
+LFS::extract_archive
+LFS::apply_patch
 
 sh Configure -des -Dprefix=/tools -Dlibs=-lm
 make
 cp -v perl cpan/podlators/pod2man /tools/bin
-mkdir -pv /tools/lib/perl5/5.20.2
-cp -Rv lib/* /tools/lib/perl5/5.20.2
+mkdir -pv /tools/lib/perl5/$ver
+cp -Rv lib/* /tools/lib/perl5/$ver
 
-cd ..
-rm -rf $pkg/
-echo "$S: You have built $pkg from scratch!"
+LFS::cleanup
 
 
