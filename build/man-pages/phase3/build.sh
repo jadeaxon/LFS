ed8f97c14b40fd0c70ea594b5f8318ede9248971
Jeff Anderson <jeff.anderson@digecor.com>
2016-04-03 19:43:38 -0700

    Add phase 3 build for man pages

---
+++ build/man-pages/phase3/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Build manpages phase 3 for LFS 7.9.
+
+# PRE: You are in the chroot environment.
+
+# NOTE: Until we have dpkg available in the chroot environment, we can't use my bash-glory package.
+
+set -e
+
+S=$(basename $0)
+src=/sources
+pkg=man-pages-4.04
+archive=$src/${pkg}.tar.xz
+
+if [ ! -e /CHROOT ]; then
+	echo "$S: ERROR: You are not in the chroot environment."
+	exit 1
+fi
+
+echo "$S: Building $pkg phase 3 (chroot)."
+
+rm -rf $pkg
+echo "$S: Extracting archive $archive."
+tar xavf $archive
+cd $pkg
+
+make install
+
+cd ..
+rm -rf $pkg
+echo "$S: WIN: You have built $pkg from scratch!"
+
+
