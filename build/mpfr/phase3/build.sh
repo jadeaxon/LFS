a164187ab9e68fe87c910db114fe24c207648a2d
Jeff Anderson <jeff.anderson@digecor.com>
2016-04-08 20:25:51 -0700

    Build mpfr phase 3

---
+++ build/mpfr/phase3/build.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Build mpfr phase 3 for LFS 7.9.
+
+# PRE: You are in the chroot environment.
+
+# NOTE: Until we have dpkg available in the chroot environment, we can't use my bash-glory package.
+
+set -e
+
+S=$(basename $0)
+src=/sources
+pkg=mpfr-3.1.3
+archive=$src/${pkg}.tar.xz
+
+if [ ! -e /CHROOT ]; then
+	echo "$S: ERROR: You are not in the chroot environment."
+	exit 1
+fi
+
+echo "$S: Building $pkg phase 3 (chroot)."
+
+rm -rf $pkg
+echo "$S: Extracting archive $archive."
+tar xavf $archive
+cd $pkg
+
+echo "$S: Patching mpfr."
+patch -Np1 -i $src/mpfr-3.1.3-upstream_fixes-2.patch
+
+./configure --prefix=/usr \
+	--disable-static \
+	--enable-thread-safe \
+	--docdir=/usr/share/doc/mpfr-3.1.3
+make
+make html
+make check
+make install
+make install-html
+
+cd ..
+rm -rf $pkg
+echo "$S: WIN: You have built $pkg from scratch!"
+
+
