1b80a1363c7abad497c67e218cf58f5f44291829
Jeff Anderson <jeff.anderson@digecor.com>
2016-04-10 15:01:45 -0600

    Build expat phase 3

---
+++ build/expat/phase3/build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# Build expat phase 3 for LFS 7.9.
+# Stream-oriented C library for parsing XML.
+
+# PRE: You are in the chroot environment.
+
+# NOTE: Until we have dpkg available in the chroot environment, we can't use my bash-glory package.
+
+set -e
+
+S=$(basename $0)
+src=/sources
+pkg=expat-2.1.0
+archive=$src/${pkg}.tar.gz
+
+if [ ! -e /CHROOT ]; then
+	echo "$S: ERROR: You are not in the chroot environment."
+	exit 1
+fi
+
+echo "$S: Building $pkg phase 3 (chroot)."
+
+rm -rf $pkg
+echo "$S: Extracting archive $archive."
+tar xavf $archive
+cd $pkg
+
+./configure --prefix=/usr --disable-static
+make
+make check
+make install
+
+install -v -dm755 /usr/share/doc/expat-2.1.0
+install -v -m644 doc/*.{html,png,css} /usr/share/doc/expat-2.1.0
+
+cd ..
+rm -rf $pkg
+echo "$S: WIN: You have built $pkg from scratch!"
+
+
