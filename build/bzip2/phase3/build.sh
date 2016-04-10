04cb1c4206bc16e124762378c43de365eaa1dd4f
Jeff Anderson <jeff.anderson@digecor.com>
2016-04-09 18:31:17 -0600

    Build bzip2 phase 3

---
+++ build/bzip2/phase3/build.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# Build bzip2 phase 3 for LFS 7.9.
+
+# PRE: You are in the chroot environment.
+
+# NOTE: Until we have dpkg available in the chroot environment, we can't use my bash-glory package.
+
+set -e
+
+S=$(basename $0)
+src=/sources
+pkg=bzip2-1.0.6
+archive=$src/${pkg}.tar.gz
+
+if [ ! -e /CHROOT ]; then
+	echo "$S: ERROR: You are not in the chroot environment."
+	exit 1
+fi
+
+echo "$S: Building $pkg phase 3 (chroot)."
+
+rm -rf $pkg
+echo "$S: Extracting archive $archive."
+tar xavf $archive
+cd $pkg
+
+echo "$S: Patching mpfr."
+patch -Np1 -i $src/bzip2-1.0.6-install_docs-1.patch
+
+# Use relative symlinks.
+sed -i 's@\(ln -s -f \)$(PREFIX)/bin/@\1@' Makefile
+
+# Put the manpages in the right spot.
+sed -i "s@(PREFIX)/man@(PREFIX)/share/man@g" Makefile
+
+make -f Makefile-libbz2_so
+make clean
+
+make
+make PREFIX=/usr install
+
+echo "$S: Creating bonus symlinks."
+cp -v bzip2-shared /bin/bzip2
+cp -av libbz2.so* /lib
+ln -sv ../../lib/libbz2.so.1.0 /usr/lib/libbz2.so
+rm -v /usr/bin/{bunzip2,bzcat,bzip2}
+ln -sv bzip2 /bin/bunzip2
+ln -sv bzip2 /bin/bzcat
+
+cd ..
+rm -rf $pkg
+echo "$S: WIN: You have built $pkg from scratch!"
+
+
