714c1a3b35927393e5bc348b9ca841898b21bb52
Jeff Anderson <jeff.anderson@digecor.com>
2016-04-09 19:58:54 -0700

    Build attr phase 3

---
+++ build/attr/phase3/build.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Build attr phase 3 for LFS 7.9.
+
+# PRE: You are in the chroot environment.
+
+# NOTE: Until we have dpkg available in the chroot environment, we can't use my bash-glory package.
+
+set -e
+
+S=$(basename $0)
+src=/sources
+pkg=attr-2.4.47
+archive=$src/${pkg}.src.tar.gz
+
+if [ ! -e /CHROOT ]; then
+	echo "$S: ERROR: You are not in the chroot environment."
+	exit 1
+fi
+
+echo "$S: Building $pkg phase 3 (chroot)."
+
+rm -rf $pkg
+echo "$S: Extracting archive $archive."
+tar xavf $archive
+cd $pkg
+
+sed -i -e 's|/@pkg_name@|&-@pkg_version@|' include/builddefs.in
+sed -i -e "/SUBDIRS/s|man[25]||g" man/Makefile
+
+./configure --prefix=/usr \
+	--bindir=/bin \
+	--disable-static
+make
+make -j1 tests root-tests
+make install install-dev install-lib
+chmod -v 755 /usr/lib/libattr.so
+mv -v /usr/lib/libattr.so.* /lib
+ln -sfv ../../lib/$(readlink /usr/lib/libattr.so) /usr/lib/libattr.so
+
+cd ..
+rm -rf $pkg
+echo "$S: WIN: You have built $pkg from scratch!"
+
+
