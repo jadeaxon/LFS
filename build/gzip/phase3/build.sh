cb1d2921dcc977f5454eac3b6bdcf1474f555c71
Jeff Anderson <jeff.anderson@digecor.com>
2016-04-12 06:27:38 -0700

    Build gzip phase 3

---
+++ build/gzip/phase3/build.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# Build gzip phase 3 for LFS 7.9.
+# GNU zip.  Compression.
+
+# PRE: You are in the chroot environment.
+
+# NOTE: Until we have dpkg available in the chroot environment, we can't use my bash-glory package.
+
+set -e
+
+S=$(basename $0)
+src=/sources
+pkg=gzip-1.6
+archive=$src/${pkg}.tar.xz
+
+if [ ! -e /CHROOT ]; then
+	echo "$S: ERROR: You are not in the chroot environment."
+	exit 1
+fi
+
+echo "$S: Building $pkg phase 3 (chroot)."
+
+rm -rf $pkg
+echo "$S: Extracting archive $archive."
+tar xavf $archive
+cd $pkg
+
+# Australia uses A4.
+./configure --prefix=/usr --bindir=/bin
+make
+make check
+make install
+
+mv -v /bin/{gzexe,uncompress,zcmp,zdiff,zegrep} /usr/bin
+mv -v /bin/{zfgrep,zforce,zgrep,zless,zmore,znew} /usr/bin
+
+cd ..
+rm -rf $pkg
+echo "$S: WIN: You have built $pkg from scratch!"
+
+
