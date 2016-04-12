094ad5f48d332ce4abc7bf287f65939b0724cc4d
Jeff Anderson <jeff.anderson@digecor.com>
2016-04-12 08:51:44 -0700

    Build sysklogd phase 3

---
+++ build/sysklogd/phase3/build.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# Build sysklogd phase 3 for LFS 7.9.
+# Daemon for logging system messages from kernel and other programs.
+
+# PRE: You are in the chroot environment.
+
+# NOTE: Until we have dpkg available in the chroot environment, we can't use my bash-glory package.
+
+set -e
+
+S=$(basename $0)
+src=/sources
+pkg=sysklogd-1.5.1
+archive=$src/${pkg}.tar.gz
+
+if [ ! -e /CHROOT ]; then
+	echo "$S: ERROR: You are not in the chroot environment."
+	exit 1
+fi
+
+echo "$S: Building $pkg phase 3 (chroot)."
+
+rm -rf $pkg
+echo "$S: Extracting archive $archive."
+tar xavf $archive
+cd $pkg
+
+sed -i '/Error loading kernel symbols/{n;n;d}' ksym_mod.c
+
+make
+make BINDIR=/sbin install
+
+cat > /etc/syslog.conf << "EOF"
+# Begin /etc/syslog.conf
+auth,authpriv.* -/var/log/auth.log
+*.*;auth,authpriv.none -/var/log/sys.log
+daemon.* -/var/log/daemon.log
+kern.* -/var/log/kern.log
+mail.* -/var/log/mail.log
+user.* -/var/log/user.log
+*.emerg *
+# End /etc/syslog.conf
+EOF
+
+cd ..
+rm -rf $pkg
+echo "$S: WIN: You have built $pkg from scratch!"
+
+
