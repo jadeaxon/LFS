6160298891408b8ee02c56ee874deca70083d9fa
Jeff Anderson <jeff.anderson@digecor.com>
2016-04-16 20:08:50 -0700

    Complete phase 3
    chroot.sh is modified not to include /tools/bin in PATH when phase 3 is
    complete.

--- chroot.sh
+++ chroot.sh
@@ -14,16 +14,25 @@ bash=/tools/bin/bash
 if [ -e $LFS/bin/bash ]; then
 	# This is the bash built in phase 3.  Should not exist until then.
 	# This path is within the chroot.
+	echo "$S: Using bash built in phase 3."
 	bash=/bin/bash
 fi
-# We have chroot call env call bash; env believes $LFS is its / dir.
-sudo chroot "$LFS" /tools/bin/env -i \
-	HOME=/root \
-	TERM="$TERM" \
-	PS1='\u:\w\$ ' \
-	PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
-	$bash --login +h
 
+if [ -e $LFS/PHASE_3_COMPLETE ]; then
+	echo "$S: Phase 3 is complete.  Not including /tools/bin in PATH."
+	sudo chroot "$LFS" /usr/bin/env -i \
+		HOME=/root TERM="$TERM" PS1='\u:\w\$ ' \
+		PATH=/bin:/usr/bin:/sbin:/usr/sbin \
+		/bin/bash --login
+else # We are still building stuff in phase 3.
+	# We have chroot call env call bash; env believes $LFS is its / dir.
+	sudo chroot "$LFS" /tools/bin/env -i \
+		HOME=/root \
+		TERM="$TERM" \
+		PS1='\u:\w\$ ' \
+		PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
+		$bash --login +h
+fi
 
 # +h tells bash not to hash program paths.
 # This makes it so that as we install final versions of commands, they'll come from /usr, etc.
